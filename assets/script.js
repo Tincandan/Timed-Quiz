27473577527e9c38300b99857e3eac81016d7b39
Tincandan <leinadoczoro333@gmail.com>
2022-09-12 20:46:28 -0700

    changed line 74 from data.json to ../data.json

--- assets/script.js
+++ assets/script.js
@@ -1,148 +1,148 @@
-        // Variables
+    // Variables
 
-        var count = 0;
-        var time = 30;
-        var marks = 0;
-        var answer = [];
-        var timer;
+    var count = 0;
+    var time = 30;
+    var marks = 0;
+    var answer = [];
+    var timer;
 
-        // Main ready function
+    // Main ready function
 
-        $(document).ready(function(){
-            $('#finish').hide();
-            $('#Result').hide();
+    $(document).ready(function(){
+        $('#finish').hide();
+        $('#Result').hide();
 
-            buttons_manager();
+        buttons_manager();
 
-            // Create function
+        // Create function
 
-            function buttons_manager(){
-                if(count > 0){
-                    $('#prev').show();
-                    if(count == 4){
-                        $('#next').hide();
-                        $('#finish').show();
-                    }
-                    else{
-                        $('#next').show();
-                    }
+        function buttons_manager(){
+            if(count > 0){
+                $('#prev').show();
+                if(count == 4){
+                    $('#next').hide();
+                    $('#finish').show();
                 }
                 else{
-                    $('#prev').hide();
+                    $('#next').show();
                 }
             }
-
-            // Create question function
-            function adding_Questions(data, i){
-                $('#questions').text(data[i.Quiz])
-                $('#options1').text(data[i].option1)
-                $('#options2').text(data[i].option2)
-                $('#options3').text(data[i].option3)
-                $('#options4').text(data[i].option4)
-                $('#number').text(Number(i+1));
-
+            else{
+                $('#prev').hide();
             }
+        }
 
-            // Answer selection function
+        // Create question function
+        function adding_Questions(data, i){
+            $('#questions').text(data[i.Quiz])
+            $('#options1').text(data[i].option1)
+            $('#options2').text(data[i].option2)
+            $('#options3').text(data[i].option3)
+            $('#options4').text(data[i].option4)
+            $('#number').text(Number(i+1));
 
-            function selected_Answer(){
-                for(var i=0 ; i<4; i++){
-                    var a = document.getElementById('options').children;
-                    if(a[i].innerHTML == answer[count]){
-                        $('#options').children('button')[i].classList.remove('active');
-                    }
+        }
+
+        // Answer selection function
+
+        function selected_Answer(){
+            for(var i=0 ; i<4; i++){
+                var a = document.getElementById('options').children;
+                if(a[i].innerHTML == answer[count]){
+                    $('#options').children('button')[i].classList.remove('active');
                 }
             }
+        }
 
-            function creating_result(data){
-                for(var i = 0 ; i<answer.length; i++){
-                    marks +=5;
-                }
-                $('#main').hide();
-
-                $('marks').text(marks);
-                $('#correct-answer').text(marks / 5);
-                $('#correct-answer').text((marks / 25)* 100 + "%");
-
-                $('#Result').show();
+        function creating_result(data){
+            for(var i = 0 ; i<answer.length; i++){
+                marks +=5;
             }
-            $('#options').hide();
+            $('#main').hide();
 
-            // Attach API
+            $('marks').text(marks);
+            $('#correct-answer').text(marks / 5);
+            $('#correct-answer').text((marks / 25)* 100 + "%");
 
-            fetch('data.json')
-            .then(function (response){
-                return response.json();
-            })
-            .then(function (data){
-                $('#btn').click(function() {
-                    $('#options').show();
-                    adding_Questions(data,Questions,count);
-                    $('#start_page').hide();
-                    $('#prev').hide();
+            $('#Result').show();
+        }
+        $('#options').hide();
 
-                    timer = setInterval(timer_function , 1000)
+        // Attach API
+
+        fetch('../data.json')
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            $('#btn').click(function() {
+                $('#options').show();
+                adding_Questions(data.Questions,count);
+                $('#start_page').hide();
+                $('#prev').hide();
+
+                timer = setInterval(timer_function , 1000)
 
                     function timer_function(){
-                        $('#time').text(time);
-                        if(timer < 1){
-                            clearInterval(timer);
-                            alert("Out of time");
-                            creating_result(data);
-                            $('#main').hide();
-                            $('#result').show();
-                        }
-                        time--;
+                    $('#time').text(time);
+                    if(timer < 1){
+                        clearInterval(timer);
+                        alert("Out of time");
+                        creating_result(data);
+                        $('#main').hide();
+                        $('#result').show();
                     }
+                    time--;
+                }
 
-                });
+            });
 
-                // Select Option
+            // Select Option
 
-                $('.option').click(function (){
+            $('.option').click(function (){
 
-                    $(this).addClass('active');
-                    $(this).siblings().removeClass('active');
-                    answer[count] = $(this).html();
-                });
+                $(this).addClass('active');
+                $(this).siblings().removeClass('active');
+                answer[count] = $(this).html();
+            });
 
-                // Next Questions
+            // Next Questions
 
-                $('#next').click(function () {
-                    if(count > answer.length -1){
-                        alert('Select at least 1 Option')
-                    }
-                    else{
-                        count++;
-                        adding_Questions(data.Questions, count);
-                        $('#prev').show();
-                        $('.option').removeClass('active');
-                        buttons_manager();
-                        selected_Answer();
-                    }
-                });
-
-                // Previous Questions
-
-                $('#prev').click(function() {
-                    count --;
+            $('#next').click(function () {
+                if(count > answer.length -1){
+                    alert('Select at least 1 Option')
+                }
+                else{
+                    count++;
                     adding_Questions(data.Questions, count);
+                    $('#prev').show();
+                    $('.option').removeClass('active');
                     buttons_manager();
                     selected_Answer();
-                });
+                }
+            });
 
-                // Finish Quiz
+            // Previous Questions
 
-                $('#finish').click(function() {
-                    if(count > answer.length -1){
-                        alert('Select at least 1 option');
-                    }
-                    else{
-                        creating_result(data);
-                        clearInterval(timer);
-                    }
-                });
-            })
+            $('#prev').click(function() {
+                count --;
+                adding_Questions(data.Questions, count);
+                buttons_manager();
+                selected_Answer();
+            });
+
+            // Finish Quiz
+
+            $('#finish').click(function() {
+                if(count > answer.length -1){
+                alert('Select at least 1 option');
+                }
+                else{
+                creating_result(data);
+                clearInterval(timer);
+            }
+        });
+    })
 
 
-        })
+})
